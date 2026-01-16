cd89b25a8254f2032798bb1103544020901cec4d
Skandhan Sivagnanam <skandhans@clouddestinations.com>
2026-01-16 18:23:21 +0530

    test sample

---
+++ build.bat
@@ -0,0 +1,19 @@
+@echo off
+echo =========================
+echo Jenkins Sample Build
+echo =========================
+echo.
+echo This is running from Windows
+echo.
+echo Auto trigger test from Jenkins
+echo.
+echo Jenkins testing
+echo.
+echo Current Directory:
+cd
+echo.
+echo Files in folder:
+dir
+echo.
+echo Build completed successfully!
+pause
